07d44de26d679aaed87824d30b0b0a2128907182
YFouquer <yohann.fouquer@lesfurets.com>
2017-10-16 17:48:25 +0200

    :construction: charts in progress:

---
+++ src/JsonParsers/chart.js
@@ -0,0 +1,12 @@
+module.exports = {
+  requestChart: function (pair_currency, period, start, end) {
+    return {
+      params: {
+        pair_currency:pair_currency,
+        period:period,
+        start:start,
+        end:end
+      }
+    };
+    }
+}
