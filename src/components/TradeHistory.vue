426a7d73cb175c409b65c497a67d1cecd150da45
FouquerYohann <yohann.fouquer@gmail.com>
2017-11-18 16:36:11 +0100

    :construction: trades is looking good

--- src/components/TradeHistory.vue
+++ src/components/TradeHistory.vue
@@ -1,37 +1,37 @@
 <template lang="html">
   <v-container>
-    <div>{{getTradeData('ETH', this.buyin,this.sellin)}}</div>
+  <div>{{getTradeData('ETH', this.buyin,this.sellin)}}</div>
     <v-layout>
-    <v-flex lg6>
+      <v-flex lg6>
       <h2 >Buying</h2>
-    <v-data-table
-     v-bind:headers="ordersHeader"
-     :items="buyin"
-     hide-actions
-     class="elevation-1"
-   >
-   <template slot="items" scope="props">
-     <td>{{ props.item.quantity }}</td>
-     <td class="text-xs-right">{{ props.item.price }}</td>
-   </template>
- </v-data-table>
-</v-flex>
-<v-flex lg6>
-  <h2>Selling</h2>
-  <v-data-table
-   v-bind:headers="ordersHeader"
-   :items="sellin"
-   hide-actions
-   class="elevation-1"
-  >
-  <template slot="items" scope="props">
-   <td>{{ props.item.quantity }}</td>
-   <td class="text-xs-right">{{ props.item.price }}</td>
-  </template>
-  </v-data-table>
-</v-flex>
-</v-layout>
-</v-container>
+        <v-data-table
+        v-bind:headers="ordersHeader"
+        :items="buyin"
+        hide-actions
+        class="elevation-1"
+        >
+          <template slot="items" scope="props">
+          <td>{{ props.item.quantity }}</td>
+          <td class="text-xs-right">{{ props.item.price }}</td>
+          </template>
+        </v-data-table>
+      </v-flex>
+      <v-flex lg6>
+      <h2>Selling</h2>
+        <v-data-table
+        v-bind:headers="ordersHeader"
+        :items="sellin"
+        hide-actions
+        class="elevation-1"
+        >
+          <template slot="items" scope="props">
+          <td>{{ props.item.quantity }}</td>
+          <td class="text-xs-right">{{ props.item.price }}</td>
+          </template>
+        </v-data-table>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 
@@ -60,16 +60,17 @@ export default {
       'v-edit-dialog': {
       }
     },
-    getTradeData(currency, buyin,sellin) {
+    getTradeData(curncy, buyin,sellin) {
       if (!this.dataLoaded) {
         axios.get(this.$store.state.urlback + this.$store.state.services.tradeHistory,
-          currency
+          {params : {currency: curncy} }
           )
           .then(function(response) {
             if (response.status)
               if (response.status == 200) {
                 for (var i = 0; i < response.data.length; i++) {
                   var tmp = response.data[i]
+                  console.error(tmp);
                   if(tmp.buying == true){
                     buyin.push(tmp)
                   }
